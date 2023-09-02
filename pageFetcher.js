e846eb1d042b1b4a392648f5da0b6cd57192d686
moogleKupo <tmaine@gmx.com>
2023-09-02 18:56:29 +0000

    refactor and add tests

--- pageFetcher.js
+++ pageFetcher.js
@@ -1,22 +1,24 @@
 const request = require("request");
 
-const breedName = "Siberian"; // Specify the breed you want to search for
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error("Error:", error);
-  } else if (response.statusCode !== 200) {
-    console.error("Request failed with status code:", response.statusCode);
-  } else {
-    const breedData = JSON.parse(body);
-
-    if (Array.isArray(breedData) && breedData.length > 0) {
-      const firstBreed = breedData[0];
-      console.log("Description:", firstBreed.description);
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+      callback(`Request failed with status code: ${response.statusCode}`, null);
     } else {
-      console.log("No breed data found for", breedName);
+      const breedData = JSON.parse(body);
+
+      if (Array.isArray(breedData) && breedData.length > 0) {
+        const firstBreed = breedData[0];
+        callback(null, firstBreed.description);
+      } else {
+        callback(`No breed data found for ${breedName}`, null);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
